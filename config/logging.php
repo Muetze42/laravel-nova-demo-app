b9ddbfc92002a6c5e57cbb84d3ae9d1ab106f3e7
Norman Huth <norman@huth.it>
2023-08-02 17:30:40 +0200

    Update dependencies

--- config/logging.php
+++ config/logging.php
@@ -73,6 +73,14 @@ return [
             'replace_placeholders' => true,
         ],
 
+        'debug' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/debug.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'days' => 14,
+            'replace_placeholders' => true,
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
